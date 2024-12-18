9e48ea1562ec623aa79a52627ac473292d2122b4
zulkiflikursani <69183966+zulkiflikursani@users.noreply.github.com>
2024-12-19 02:44:24 +0800

    update responsive

--- src/app/component/Products.tsx
+++ src/app/component/Products.tsx
@@ -3,9 +3,9 @@ import React from "react";
 
 function Products() {
   return (
-    <section id="product" className="min-h-screen my-5">
-      <div className="flex items-center justify-center">
-        <div className="flex flex-col justify-center items-center w-10/12">
+    <section id="product" className="min-h-screen w-full mb-4">
+      <div className="flex min-h-screen items-center justify-center">
+        <div className="grid justify-center items-center w-10/12">
           <div className="flex flex-col justify-items-center text-center">
             <h1 className="text-3xl mb-3 text-primary-light font-bold">
               Our Product
@@ -18,9 +18,9 @@ function Products() {
               fugiat accusamus doloribus quis accusantium ab at unde dolorum
               possimus.
             </h1>
-            <div className="grid gap-10 md:grid-cols-3 grid-cols-1 justify-items-center ">
-              <div className="flex gap-3">
-                <div className="flex space-y-2 py-5 flex-col items-center justify-center bg-secondary-light w-[310px] p-2 text-center rounded-3xl min-h-2">
+            <div className="grid gap-10 md:grid-cols-3 grid-cols-1 justify-items-center  ">
+              <div className="grid grid-cols-1 gap-3">
+                <div className="grid grid-cols-1 space-y-2 py-5 flex-col items-center justify-items-center bg-secondary-light w-[310px] p-2 text-center rounded-3xl min-h-2">
                   <Image
                     src={"/image/produks/tabungan.png"}
                     alt={"gambar"}
@@ -40,8 +40,8 @@ function Products() {
                   </button>
                 </div>
               </div>
-              <div className="flex gap-3">
-                <div className="flex space-y-2 py-5 flex-col items-center justify-center bg-secondary-light w-[310px] p-2 text-center rounded-3xl min-h-2">
+              <div className="grid grid-cols-1 gap-3">
+                <div className="grid grid-cols-1 space-y-2 py-5 flex-col items-center justify-items-center  bg-secondary-light w-[310px] p-2 text-center rounded-3xl min-h-2">
                   <Image
                     src={"/image/produks/pembiayaan.png"}
                     alt={"gambar"}
@@ -61,8 +61,8 @@ function Products() {
                   </button>
                 </div>
               </div>
-              <div className="flex gap-3">
-                <div className="flex space-y-2 py-5 flex-col items-center justify-center bg-secondary-light w-[310px] p-2 text-center rounded-3xl min-h-2">
+              <div className="grid grid-cols-1 gap-3">
+                <div className="grid grid-cols-1 space-y-2 py-5 flex-col items-center justify-items-center  bg-secondary-light w-[310px] p-2 text-center rounded-3xl min-h-2">
                   <Image
                     src={"/image/produks/deposito.png"}
                     alt={"gambar"}
