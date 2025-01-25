0a3bff5de7e7e360f74c4fb0a27a41188547c7e6
zulkiflikursani <69183966+zulkiflikursani@users.noreply.github.com>
2025-01-25 16:37:58 +0800

    update pengembang

---
+++ src/app/about-dev/page.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+function page() {
+  return (
+    <div className="bg-gray-100 w-full flex flex-col items-center py-10 -mt-4">
+      <h1 className="text-4xl font-bold">Tim Pengembang: </h1>
+      <p className="text-2xl font-bold mt-3 underline">
+        Manajer Analis Sistem:
+      </p>
+      <p className="text-xl font-bold"> Dr. Dinar, S.E., M.Si</p>
+      <p className="text-xl ">
+        (Dosen Program Studi Akuntansi Universitas Fajar)
+      </p>
+      <p className="text-2xl font-bold mt-3 underline">
+        Analis Sistem & Desain:
+      </p>
+      <p className="text-xl font-bold">Akmal Hidayat, S.E., M.Si </p>
+      <p className="text-xl ">
+        (Dosen Program Studi Akuntansi Universitas Fajar)
+      </p>
+      <p className="text-2xl font-bold mt-3 underline">Programmer: </p>
+      <p className="text-xl font-bold ">Zulkifli, S.ST., M.Si </p>
+      <p className="text-xl ">
+        (Dosen Program Studi Akuntansi Universitas Fajar)
+      </p>
+    </div>
+  );
+}
+
+export default page;
